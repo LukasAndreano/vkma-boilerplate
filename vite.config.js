ffbbe6bb7e8d358f682a11db807c54e97292aebb
Lukas Andreano <business.nikitap@gmail.com>
2022-12-20 21:08:27 +0600

    feat: svg support

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import svgLoader from "vite-svg-loader";
 
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react(), process?.env?.ODR ? viteSingleFile() : null],
+    plugins: [
+      react(),
+      svgLoader(),
+      process?.env?.ODR ? viteSingleFile() : null,
+    ],
     optimizeDeps: {
       include: ["recoil"],
     },
